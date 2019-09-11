d96a5ba8ef9126ebfb8477a145fdde2c41beba95
nim <nimisha.k.vijay@gmail.com>
2019-09-12 00:09:27 +0530

    added home page which redirects to desired dashboard

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { HomePage } from './home.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  }
+];
 
 @NgModule({
   imports: [
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+	isLoading: boolean = true;
+	currentUser: any = {
+		key: '',
+		state: ''
+	};
+
+	constructor (
+		private router: Router
+	) { }
+
+	async ngOnInit() {
+		firebase.auth().onAuthStateChanged(async user => {
+			if (user) {
+				if (firebase.auth().currentUser.email.indexOf("unx.life") !== -1) {
+					this.router.navigate(['/adminhome'])
+				}
+				else {
+					await firebase.database().ref("users/" + firebase.auth().currentUser.uid + "/currentState").once("value", snapshot => {
+							this.currentUser.state = snapshot.val();
+					}).then(() => console.log("type: ", this.currentUser.state));
+					if (this.currentUser.state === "mentor") this.router.navigate(['/mentorhome'])
+					else if (this.currentUser.state === "mentee") this.router.navigate(['/menteehome'])
+					else console.log("error");
+				}
+			} else this.router.navigate(["/signin"]); 
+		})
+	}
 
 }
